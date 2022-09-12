44b870bc475c59447a50b790d9dbb9db579ac8d5
Nikita Tsukanov <ataka.tsukanov@gmail.com>
2022-09-12 11:21:00 +0300

    create user profile block

--- src/App.js
+++ src/App.js
@@ -5,9 +5,16 @@ import Maincontent from "./components/maincontent/Maincontent";
 
 function App() {
     return (
-        <div className="container">
-            <Sidebar />
-            <Maincontent />
+        <div className="app">
+            <div className="header">
+                <div className="logo">HAYF</div>
+            </div>
+            <div className="container">
+                <div className="content-wrapper">
+                  <Sidebar />
+                  <Maincontent /> 
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -3,7 +3,18 @@ import './Sidebar.css';
 
 const Sidebar = () => {
     return (
-        <div className="sidebar"></div>
+        <div className="sidebar">
+            <div className="user-profile">
+                <img className="userpic" src="https://resheto.net/images/mater/pozitivnye_kartinki_2.jpg"></img>
+                <div className="userName">
+                    <div className="name">Никита Цуканов</div>
+                    <div className="login">@itwouldseemthat</div>
+                    <div className="birthsday"><span className="bd">День рождения: </span>23 сентября 2000</div>
+                </div>
+                <div className="lane"></div>
+                <button className="edit-profile" type="submit">Edit profile</button>
+            </div>
+        </div>
     )
 }
 
