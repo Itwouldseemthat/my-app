ae37f28281a188f00b5ed000c890ccc95860034f
Nikita Tsukanov <ataka.tsukanov@gmail.com>
2022-09-15 14:56:51 +0300

    try create chart

--- src/components/graphic/Graphic.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import './Graphic.css';
-
-const Graphic = () => {
-}
-
-export default Graphic;
--- src/components/maincontent/Maincontent.js
+++ src/components/maincontent/Maincontent.js
@@ -1,12 +1,19 @@
-import React from "react";
-import Graphic from "../graphic/Graphic";
+import React, { Component } from "react";
+import Chart from "../chart/Chart";
 import './Maincontent.css';
 
-const Maincontent = () => {
-    return (
-        <div className="maincontent background">
-        </div>
-    )
+// const Maincontent = () => {
+//     return (
+//         <div className="maincontent background">
+//             <Chart />
+//         </div>
+//     )
+// }
+
+class Maincontent extends Component {
+    render() {
+      return <Chart />
+    }
 }
 
 export default Maincontent;
